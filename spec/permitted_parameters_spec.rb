448733e4ae29b679f75719f02168907c0d69298a
Takeshi Yabe <tyabe@nilidea.com>
2012-11-26 03:41:04 +0900

    refactor for rspec

--- spec/permitted_parameters_spec.rb
+++ spec/permitted_parameters_spec.rb
@@ -3,60 +3,69 @@ require 'sinatra/filtering_parameters'
 require 'json'
 
 describe Sinatra::FilteringParameters do
-  let(:pass_params) { { :a => 1, :b => 2, :c => 3 } }
 
-  before do
-    result =  Proc.new { params.to_json }
+  def post_with_filter(args)
     mock_app do
       register Sinatra::FilteringParameters
-      post '/sample/:name', &result
-      post '/symbol/:name', allow: :name, &result
-      post '/string/:name', allow: 'name', &result
-      post '/array/:name', allow: [:name, :a], &result
-      post '/empty_string/:name', allow: '', &result
-      post '/empty_array/:name', allow: [], &result
+      if args[:allow].nil?
+        post('/sample/:name'){ params.to_json }
+      else
+        post('/sample/:name', allow: args[:allow]){ params.to_json }
+      end
     end
+    post '/sample/foo', args[:pass_params]
+  end
+
+  def result_should_be_equal(filterd_params)
+    last_response.should be_ok
+    last_response.body.should == filterd_params.to_json
   end
 
   describe 'permitted parameters nothing' do
     it "when success" do
-      post '/sample/foo', pass_params
-      last_response.should be_ok
-      last_response.body.should ==
-        {"a"=>"1", "b"=>"2", "c"=>"3", "splat"=>[], "captures"=>["foo"], "name"=>"foo"}.to_json
+      post_with_filter(
+        :pass_params  =>  { :a => 1, :b => 2, :c => 3 }
+      )
+      result_should_be_equal({ "a"=>"1", "b"=>"2", "c"=>"3", "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
     end
   end
+
   describe 'permitted parameters specified in' do
     it 'symbol' do
-      post '/symbol/foo', pass_params
-      last_response.should be_ok
-      last_response.body.should ==
-        {"splat"=>[], "captures"=>["foo"], "name"=>"foo"}.to_json
+      post_with_filter(
+        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
+        :allow        =>  :name
+      )
+      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
     end
     it "string" do
-      post '/string/foo', pass_params
-      last_response.should be_ok
-      last_response.body.should ==
-        {"splat"=>[], "captures"=>["foo"], "name"=>"foo"}.to_json
+      post_with_filter(
+        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
+        :allow        =>  'name'
+      )
+      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
     end
     it "array" do
-      post '/array/foo', pass_params
-      last_response.should be_ok
-      last_response.body.should ==
-        {"splat"=>[], "captures"=>["foo"], "name"=>"foo", "a"=>"1"}.to_json
+      post_with_filter(
+        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
+        :allow        =>  [:name, :a]
+      )
+      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo", "a"=>"1" })
     end
     context "empty" do
       it "string" do
-        post '/empty_string/foo', pass_params
-        last_response.should be_ok
-        last_response.body.should ==
-          {"splat"=>[], "captures"=>["foo"]}.to_json
+        post_with_filter(
+          :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
+          :allow        =>  ''
+        )
+        result_should_be_equal({ "splat"=>[], "captures"=>["foo"] })
       end
       it "array" do
-        post '/empty_array/foo', pass_params
-        last_response.should be_ok
-        last_response.body.should ==
-          {"splat"=>[], "captures"=>["foo"]}.to_json
+        post_with_filter(
+          :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
+          :allow        =>  []
+        )
+        result_should_be_equal({ "splat"=>[], "captures"=>["foo"] })
       end
     end
   end
