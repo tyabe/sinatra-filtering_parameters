75b4ceb621a8cae6828afddc2ad971cfc988fa5f
Takeshi Yabe <tyabe@nilidea.com>
2012-11-27 02:09:26 +0900

    add nested parameters pattern

--- lib/sinatra/filtering_parameters.rb
+++ lib/sinatra/filtering_parameters.rb
@@ -22,12 +22,46 @@ module Sinatra
         _filters = [filters].flatten
         [filters].flatten.each do |filter|
           _filters.shift
-          filter = filter.to_s
-          if params.has_key?(filter)
-            if params[filter].is_a?(Hash)
-              allow_params[filter] = allow(params[filter], _filters)
-            else
-              allow_params[filter] = params[filter]
+          case filter
+          when Symbol, String
+            filter = filter.to_s
+            next unless params.is_a?(Hash)
+
+            if params.has_key?(filter)
+              if params[filter].is_a?(Hash)
+                allow_param = allow(params[filter], _filters)
+                allow_params[filter] = allow_param unless allow_param.empty?
+              else
+                allow_params[filter] = params[filter]
+              end
+            end
+          when Hash
+            _params = {}
+            filter.keys.map(&:to_s).each { |k| _params[k] = params[k] if params.has_key?(k) }
+            _params.each do |key, value|
+              case value
+              when Array
+                [value].flatten.each do |v|
+                  allow_param = allow(v, filter.values)
+                  unless allow_param.empty?
+                    allow_params[key] ||= []
+                    allow_params[key] << allow_param
+                  end
+                end
+              else
+                if value.keys.all? { |k| k =~ /\A-?\d+\z/ }
+                  value.each do |k, v|
+                    allow_param = allow(v, filter.values)
+                    unless allow_param.empty?
+                      allow_params[key] ||= {}
+                      allow_params[key][k] = allow_param
+                    end
+                  end
+                else
+                  allow_param = allow(value, filter.values)
+                  allow_params[key] = allow_param unless allow_param.empty?
+                end
+              end
             end
           end
         end
---
+++ spec/nested_parameters_spec.rb
@@ -0,0 +1,213 @@
+require 'spec_helper'
+
+describe Sinatra::FilteringParameters do
+
+  def post_with_filter(args)
+    mock_app do
+      register Sinatra::FilteringParameters
+      post('/', allow: args[:allow]){ params.to_json }
+    end
+    post '/', args[:pass_params]
+  end
+
+  it "permitted nested parameters" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :title => "Romeo and Juliet",
+          :authors => [{
+            :name => "William Shakespeare",
+            :born => "1564-04-26"
+          }, {
+            :name => "Christopher Marlowe"
+          }],
+          :details => {
+            :pages => 200,
+            :genre => "Tragedy"
+          }
+        },
+        :magazine => "Mjallo!"
+      },
+      :allow => [
+        :book => [
+          :title,
+          :authors => :name,
+          :details => :pages
+        ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "title" => "Romeo and Juliet",
+        "authors" => [{
+          "name" => "William Shakespeare"
+        },{
+          "name" => "Christopher Marlowe"
+        }],
+        "details" => {
+          "pages" => "200"
+        }
+      }
+    })
+  end
+  it "nested arrays with strings" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :genres => ["Tragedy"]
+        }
+      },
+      :allow => [
+        :book => :genres
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "genres" => ["Tragedy"]
+      }
+    })
+  end
+
+  it "permit may specify symbols or strings" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :title => "Romeo and Juliet",
+          :author => "William Shakespeare"
+        },
+        :magazine => "Shakespeare Today"
+      },
+      :allow => [{
+        :book => [
+          "title",
+          :author
+        ]},
+        "magazine"
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "title" => "Romeo and Juliet",
+        "author" => "William Shakespeare",
+      },
+      "magazine" => "Shakespeare Today"
+    })
+  end
+
+  it "nested array with strings that should be hashes" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :genres => ["Tragedy"]
+        }
+      },
+      :allow => [
+        :book => [
+          :genres => :type
+        ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil
+    })
+  end
+
+  it "nested array with strings that should be hashes and additional values" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :title => "Romeo and Juliet",
+          :genres => ["Tragedy"]
+        }
+      },
+      :allow => [
+        :book => [
+          :title,
+          :genres => :type
+        ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "title" => "Romeo and Juliet"
+      }
+    })
+  end
+
+  it "nested string that should be a hash" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :genre => "Tragedy"
+        }
+      },
+      :allow => [
+        :book => [
+          :genres => :type
+         ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil
+    })
+  end
+
+  it "fields_for_style_nested_params" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :authors_attributes => {
+            :'0' => { :name => 'William Shakespeare', :age_of_death => '52' },
+            :'1' => { :name => 'Unattributed Assistant' }
+          }
+        }
+      },
+      :allow => [
+        :book => [
+          :authors_attributes => :name
+        ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "authors_attributes" => {
+          "0" => { "name" => "William Shakespeare" },
+          "1" => { "name" => "Unattributed Assistant" }
+        }
+      }
+    })
+  end
+
+  it "fields_for_style_nested_params with negative numbers" do
+    post_with_filter(
+      :pass_params => {
+        :book => {
+          :authors_attributes => {
+            :'-1' => { :name => 'William Shakespeare', :age_of_death => '52' },
+            :'-2' => { :name => 'Unattributed Assistant' }
+          }
+        }
+      },
+      :allow => [
+        :book => [
+          :authors_attributes => :name
+        ]
+      ]
+    )
+    result_should_be_equal({
+      "splat" => nil, "captures" => nil,
+      "book" => {
+        "authors_attributes" => {
+          "-1" => { "name" => "William Shakespeare" },
+          "-2" => { "name" => "Unattributed Assistant" }
+        }
+      }
+    })
+  end
+
+end
