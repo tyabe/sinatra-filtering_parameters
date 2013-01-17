81039de55ea64f3a7fbedc6d73161823590bbf50
Takeshi Yabe <tyabe@nilidea.com>
2013-01-18 03:12:23 +0900

    Refactor for spec of permitted parameters

--- spec/permitted_parameters_spec.rb
+++ spec/permitted_parameters_spec.rb
@@ -2,65 +2,71 @@ require 'spec_helper'
 
 describe Sinatra::FilteringParameters do
 
-  def post_with_filter(args)
+  def mock_route(path, opts={}, &block)
     mock_app do
       register Sinatra::FilteringParameters
-      if args[:allow].nil?
-        post('/sample/:name'){ params.to_json }
-      else
-        post('/sample/:name', allow: args[:allow]){ params.to_json }
+      %w(get post put delete head options patch).each do |verb|
+        send(verb.to_sym, path, opts, &block)
       end
-    end
-    post '/sample/foo', args[:pass_params]
-  end
-
-  describe 'permitted parameters nothing' do
-    it "when success" do
-      post_with_filter(
-        :pass_params  =>  { :a => 1, :b => 2, :c => 3 }
-      )
-      result_should_be_equal({ "a"=>"1", "b"=>"2", "c"=>"3", "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
     end
   end
 
-  describe 'permitted parameters specified in' do
-    it 'symbol' do
-      post_with_filter(
-        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
-        :allow        =>  :name
-      )
-      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
-    end
-    it "string" do
-      post_with_filter(
-        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
-        :allow        =>  'name'
-      )
-      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo" })
-    end
-    it "array" do
-      post_with_filter(
-        :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
-        :allow        =>  [:name, :a]
-      )
-      result_should_be_equal({ "splat"=>[], "captures"=>["foo"], "name"=>"foo", "a"=>"1" })
-    end
-    context "empty" do
-      it "string" do
-        post_with_filter(
-          :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
-          :allow        =>  ''
-        )
-        result_should_be_equal({ "splat"=>[], "captures"=>["foo"] })
+  describe 'support matching parameters' do
+    it 'for splat' do
+      mock_route '/say/*/to/*' do
+        params[:splat].should == ['hello','world']
       end
-      it "array" do
-        post_with_filter(
-          :pass_params  =>  { :a => 1, :b => 2, :c => 3 },
-          :allow        =>  []
-        )
-        result_should_be_equal({ "splat"=>[], "captures"=>["foo"] })
-      end
+      get '/say/hello/to/world'
     end
+
+    it 'for captures' do
+      mock_route %r{/hello/([\w]+)} do
+        params[:captures].should == ['world']
+      end
+      get '/hello/world'
+    end
+  end
+
+  context 'do not specify ilter' do
+    it "does not remove anything from the parameter" do
+      mock_route '/pages/:id' do
+        params[:id].should == '1'
+        params[:foo].should == 'foo'
+        params[:bar].should == 'bar'
+        params[:baz].should == 'baz'
+        params[:captures].should == ['1']
+        params[:splat].should == []
+      end
+      post '/pages/1', :foo => 'foo', :bar => 'bar', :baz => 'baz'
+    end
+  end
+
+  context 'will be only parameters that allowed in' do
+    def should_with(filter={})
+      mock_route '/pages', filter do
+        params.delete('foo').should == 'foo'
+        params.each { |param| param.should == nil }
+      end
+      post '/pages', :foo => 'foo', :bar => 'bar', :baz => 'baz'
+    end
+
+    it('symbol'){ should_with :allow => :foo }
+    it("string"){ should_with :allow => 'foo' }
+    it("array") { should_with :allow => [:foo] }
+  end
+
+  context 'parameter will be empty when specify in' do
+    def should_with(filter={})
+      mock_route '/pages', filter do
+        params.each { |param| param.should == nil }
+      end
+      post '/pages', :foo => 'foo', :bar => 'bar', :baz => 'baz'
+    end
+
+    it("blank string"){ should_with :allow => '' }
+    it("empty array") { should_with :allow => [] }
+    it("empty hash")  { should_with :allow => {} }
+    it("nil")         { should_with :allow => nil }
   end
 
 end
